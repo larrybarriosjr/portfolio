6d9fb2841fb715b5f6d371dd3aba0ff93fefcc3f
Larry Barrios Jr <github@larrybarriosjr.com>
2021-06-06 16:45:07 +0800

    Move icon classes to icon context

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,17 +1,20 @@
 import Button from "components/common/Button"
 import Logo from "components/layout/Logo"
 import Row from "containers/Row"
+import { IconContext } from "react-icons"
 import { BiEnvelope, BiSearchAlt } from "react-icons/bi"
 import Drawer from "./Drawer"
 
 const Header = () => {
   return (
-    <Row className="items-center">
-      <Logo className="mr-auto" />
-      <Button icon={<BiSearchAlt />} className="mx-3 text-4xl text-forest-green" />
-      <Button icon={<BiEnvelope />} className="mx-3 text-4xl text-forest-green" />
-      <Drawer />
-    </Row>
+    <IconContext.Provider value={{ className: "text-4xl text-forest-green" }}>
+      <Row className="items-center">
+        <Logo className="mr-auto" />
+        <Button icon={<BiSearchAlt />} className="mx-3" />
+        <Button icon={<BiEnvelope />} className="mx-3" />
+        <Drawer />
+      </Row>
+    </IconContext.Provider>
   )
 }
 
