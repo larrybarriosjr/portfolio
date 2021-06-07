b781718944f3554d27e9326b4bc501e3631fc293
Larry Barrios Jr <github@larrybarriosjr.com>
2021-06-07 20:02:06 +0800

    Add Skills page

---
+++ pages/skills/index.tsx
@@ -0,0 +1,46 @@
+import Text from "components/common/Text"
+import Title from "components/layout/Title"
+import Row from "containers/Row"
+import Section from "containers/Section"
+import Image from "next/image"
+import Link from "next/link"
+import personal from "public/images/personal-skills.svg"
+import technical from "public/images/technical-skills.svg"
+import { Fragment } from "react"
+import { TextType } from "types/enum"
+
+const Skills = () => {
+  return (
+    <Fragment>
+      <Title
+        label="Skills"
+        heading="Skills"
+        subheading="This is a description of the skills list. What can usually be found here."
+      />
+      <Section className="pb-4">
+        <Row className="flex-col text-center">
+          <Row className="relative h-60 lg:h-80">
+            <Image src={technical} layout="fill" objectFit="contain" />
+          </Row>
+          <Link href="/skills/technical">
+            <Text type={TextType.ITEM_NAME} link>
+              Technical Skills
+            </Text>
+          </Link>
+        </Row>
+        <Row className="flex-col text-center">
+          <Row className="relative h-60 lg:h-80">
+            <Image src={personal} layout="fill" objectFit="contain" />
+          </Row>
+          <Link href="/skills/personal">
+            <Text type={TextType.ITEM_NAME} link>
+              Personal Skills
+            </Text>
+          </Link>
+        </Row>
+      </Section>
+    </Fragment>
+  )
+}
+
+export default Skills
