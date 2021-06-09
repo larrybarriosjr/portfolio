2a386f7a92e76729fa6c7de0c5222d0791cbeffb
Larry Barrios Jr <github@larrybarriosjr.com>
2021-06-09 16:53:50 +0800

    Add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,22 @@
 import Header from "components/layout/Header"
 import Nav from "components/layout/Nav"
 import type { AppProps } from "next/app"
+import Head from "next/head"
 import "styles/globals.css"
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <div className="w-full min-h-screen bg-steel-grey">
       <div className="w-full max-w-5xl min-h-screen pt-px mx-auto lg:px-2 bg-seafoam-green">
+        <Head>
+          <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
+        </Head>
         <Header />
         <main className="pb-14 lg:pb-px">
           <Component {...pageProps} />
